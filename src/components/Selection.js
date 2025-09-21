354e4c84b8b36657832ede86f71cbf886b700cc4
Ishan Riyal <ishanriyal07@gmail.com>
2025-09-22 01:25:35 +0530

    final comit

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
+import React, { useState } from "react";
+import "../styles/child.css"; // make sure the path is correct
 
+const Selection = ({ applyColor }) => {
+  const [style, setStyle] = useState({ background: "" });
+
+  return (
+    <div
+      className="fix-box"
+      style={style}
+      onClick={() => applyColor(setStyle)}
+    ></div>
+  );
+};
+
+export default Selection;
